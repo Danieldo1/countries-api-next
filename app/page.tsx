f6bf89031ef65805872c65752ef40b615d98370d
Dan <daniel.speranskiy@live.com>
2023-10-31 17:09:16 +0400

    Added flags and names in their containers

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,35 @@
-import Image from 'next/image'
+import Image from "next/image"
 
-export default function Home() {
+type Country = {
+  name:{
+    common: string
+  }
+  flags:{
+    png: string;
+    alt: string;
+  }
+}
+
+async function getData(): Promise<Country[]>  {
+  const res = await fetch('https://restcountries.com/v3.1/all')
+  return res.json()
+}
+
+export default async function Home() {
+  const countries = await getData()
   return (
-    <main>
-      
-    </main>
+    <section className='grid grid-cols-5 w-full container gap-2 mt-16'>
+        {countries.map((country) => (
+          <article className=" min-w-full p-2 bg-white border-2 rounded-2xl hover:border-blue-500 "
+          key={country.name.common}
+          >
+            <div className="w-full p-2 overflow-hidden">
+              <Image src={country.flags.png} alt={country.name.common} width={100} height={100} className="rounded-xl object-cover" />
+            </div>
+
+            <h1 className="font-bold text-xl text-center mt-1">{country.name.common}</h1>
+          </article>
+        ))}
+    </section>
   )
 }
